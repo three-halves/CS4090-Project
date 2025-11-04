aff9531c56c46b8b4d330023e60bedfa3cda6815
Brendan Westley <brendan@westley.us>
2025-11-04 01:14:45 -0600

    KAN-15 Access Event via Link

---
+++ Db.cs
@@ -0,0 +1,95 @@
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Db
+{
+    public class DatabaseContext(DbContextOptions<DatabaseContext> options) : DbContext(options)
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Event> Events { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=CS4090.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasMany(e => e.Organizing)
+                .WithOne(e => e.Organizer)
+                .HasForeignKey(e => e.OrganizerId)
+                .IsRequired();
+            modelBuilder.Entity<User>()
+                .HasMany(e => e.Attending)
+                .WithMany(e => e.Attendees)
+                .UsingEntity<Attendance>();
+            modelBuilder.Entity<User>().HasData(new User
+            {
+                Id = Guid.Parse("fc771b9e-2a04-42a6-b73a-714d6ddc3feb"),
+                Name = "Test User",
+                PasswordHash = "",
+                Organizing = [],
+                Attending = [],
+                Attendance = []
+            });
+            modelBuilder.Entity<Event>().HasData(new Event
+            {
+                Id = Guid.Parse("4ec07ab7-5385-475c-b727-3bf5beda74ed"),
+                Title = "Test Event",
+                Description = "Test Description",
+                DaysOfTheWeek = false,
+                Mask = [],
+                FinalizedStart = null,
+                FinalizedEnd = null,
+                OrganizerId = Guid.Parse("fc771b9e-2a04-42a6-b73a-714d6ddc3feb"),
+                Attendees = [],
+            });
+        }
+    }
+
+    public class User
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Name { get; set; } = "";
+        public string PasswordHash { get; set; } = "";
+
+        public List<Event> Organizing { get; set; } = [];
+
+        public List<Event> Attending { get; set; } = [];
+        public List<Attendance> Attendance { get; set; } = [];
+    }
+
+    public class Event
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Title { get; set; } = "";
+        public string Description { get; set; } = "";
+        public bool DaysOfTheWeek { get; set; } = false;
+        public List<DateTime> Mask { get; set; } = [];
+        public Privacy Privacy { get; set; } = Privacy.PUBLIC_IDENTIFIED;
+        public DateTime? FinalizedStart { get; set; } = null;
+        public DateTime? FinalizedEnd { get; set; } = null;
+
+        public Guid OrganizerId { get; set; }
+        public User Organizer { get; set; }
+
+        public List<User> Attendees { get; set; } = [];
+        public List<Attendance> Attendance { get; set; } = [];
+    }
+
+    public class Attendance
+    {
+        public Guid UserId { get; set; }
+        public Guid EventId { get; set; }
+        public List<DateTime> Availability { get; set; } = [];
+    }
+
+    public enum Privacy
+    {
+        PRIVATE, PUBLIC_CUMULATIVE, PUBLIC_IDENTIFIED
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,28 +1,21 @@
 using CS4090_Project.Components;
+using Db;
+using Microsoft.EntityFrameworkCore;
+
+using (var context = new DatabaseContext(new DbContextOptions<DatabaseContext>())) { context.Database.EnsureCreated(); }
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddDbContextFactory<DatabaseContext>();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-
-
+app.UseAuthentication();
+app.UseAuthorization();
 app.UseAntiforgery();
-
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
-
 app.Run();
